53985157ca773a6fcf1505a7e44011e019966f1f
Franz <francesco.romeo-work@libero.it>
2024-01-27 14:42:24 +0100

    Added Pickup & Level Reset on Fall

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 using UnityEngine.XR;
 
 public class PlayerController : MonoBehaviour
 {
     // Variabili per la gestione del movimento e del salto
     public float movementSpeed = 1.0f;
+    public float maxMovementSpeed = 5.0f;
     public float jumpForce = 10.0f;
     //public float runningSpeedMultiplier = 3.0f;
     public Transform spawn;
+    public GameObject pickUpHint;
 
     // Nomi degli assi di input per il movimento
     private string horizontalName = "Horizontal", verticalName = "Vertical";
@@ -24,8 +27,11 @@ public class PlayerController : MonoBehaviour
     // Flag che indica se il giocatore è a contatto con il terreno
     private bool isGrounded;
 
-    // Contatore per il numero di dash disponibili
+    // Contatore per il numero di doppi salti disponibili
     private int dashCount = 1;
+
+    private GameObject pickable;
+    private bool keyPicked;
 
     // Start è chiamato prima del primo frame
     void Start()
@@ -38,7 +44,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // Verifica se il giocatore è a contatto con il terreno
-        isGrounded = Physics.Raycast(transform.position, Vector3.down, 1.01f);
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, 0.51f);
 
         // Ottieni i valori dell'input orizzontale e verticale
         horizontalValue = Input.GetAxis(horizontalName);
@@ -65,19 +71,27 @@ public class PlayerController : MonoBehaviour
             Debug.Log("Jump");
             Jump();
         }
-        // Gestisci il dash in aria
+        // Gestisci il doppio salto
         else if (!isGrounded && dashCount > 0 && (Input.GetKeyDown(KeyCode.Space) || Input.GetButtonDown("Jump")))
         {
             Dash();
             dashCount--;
         }
-
         // Riporta il Player allo Spawn
-        if(Input.GetKeyDown(KeyCode.R))
+        else if(Input.GetKeyDown(KeyCode.R))
         {
             Debug.Log("Spawn");
             body.Move(spawn.position, Quaternion.identity);
             body.velocity = Vector3.zero;
+        }
+        else if (Input.GetKeyDown(KeyCode.E) && pickable)
+        {
+            pickable.SetActive(false);
+            pickUpHint.SetActive(false);
+            if (pickable.name == "Key")
+            {
+                keyPicked = true;
+            }
         }
 
         //body.velocity = new Vector3(horizontalValue, body.velocity.y, body.velocity.z);
@@ -88,7 +102,37 @@ public class PlayerController : MonoBehaviour
     {
         // Applica la forza per il movimento laterale
         body.AddForce(new Vector3(horizontalValue * movementSpeed, 0, 0), ForceMode.Force);
-        Debug.Log("horizontalvalue " + horizontalValue);
+        //body.velocity.x = Mathf.Clamp(body.velocity.x, -maxMovementSpeed, maxMovementSpeed);
+        if(Mathf.Abs(body.velocity.magnitude) > maxMovementSpeed)
+        {
+            body.velocity = body.velocity.normalized * maxMovementSpeed;
+        }
+        Debug.Log(body.velocity);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Pickup"))
+        {
+            pickable = other.gameObject;
+            pickUpHint.SetActive(true);
+        }
+        else
+        {
+            if(other.CompareTag("Reset"))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Pickup"))
+        {
+            pickable = null;
+            pickUpHint.SetActive(false);
+        }
     }
 
     public void Move(InputAction.CallbackContext context)
@@ -108,4 +152,5 @@ public class PlayerController : MonoBehaviour
         body.velocity = new Vector3(body.velocity.x, 0f, 0f);
         body.AddForce(new Vector3(0, jumpForce / 1.5f, 0), ForceMode.Impulse);
     }
+
 }
